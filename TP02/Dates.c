9830851515b5c945649d6847c1ac4810fd4d07e1
TimoFGT <timothe.fanget@gmail.com>
2024-09-16 11:58:26 +0200

    Commit de midi

--- TP02/Dates.c
+++ TP02/Dates.c
@@ -1,4 +1,8 @@
-enum mois {
+#include <stdio.h>
+#include <stdbool.h>
+
+// Types
+typedef enum {
     janvier,
     février,
     mars,
@@ -11,29 +15,77 @@ enum mois {
     octobre,
     novembre,
     decembre,
-};
+} Mois;
 
-struct Date {
+typedef struct {
     int jour;
-    enum mois Mois;
+    Mois mois;
     int annee;
-};
+} Date;
 
-void main(void) {
-    struct Date d;
+
+// Fonctions
+
+bool estBissextile(short annee);
+
+bool estDateValide(Date* d);
+short nombreDeJoursDansLeMois(Date *d);
+bool isDateValide(Date *d);
+char* libelleMois(Mois m);
+
+void afficherDate(Date *d);
+void initialiseDate(Date *d);
+Date* newDate();
+
+
+int main(void) {
+    Date d;
+    printf("Version 1: initialisation d'une date allouée statiquement\n");
     initialiseDate(&d);
     afficheDate(&d);
+
+    printf("\nVersion 2: recopie d'une date alloué statiquement\n");
     d = creerDateParCopie();
+    afficheDate(&d);
+
+    printf("\nVersion 3: allocation dynamique de date\n");
+    Date *pDate;
+    pDate = newDate();
+    afficherDate(pDate);
+    free(pDate);
+    pDate=NULL;
+
+    printf("\nVersion 3.1: allocation dynamique d'un tableau de date\nCette dernière version sera celle à privilégier notamment dans les prochains TD sur les matrices ou les listes\n");
 };
 
-void initialiseDate(struct Date *d) {
-    int CeJour;
-    enum mois CeMois;
-    int CetteAnnee;
-    scanf("%d %s %d", CeJour, CeMois, CetteAnnee);
+void initialiseDate(Date *d) {
+    bool dValide = false;
+
+    do {
+        printf("jour: ");
+        scanf("%hu",&(d->jour));
+        printf("mois: ");
+        scanf("%d",(int *)&d->mois);
+        printf("annee: ");
+        scanf("%hu",&d->annee);
+
+        if(!(dValide=estDateValide(d))) {
+            printf("\t date invalide !");
+            afficherDate(d);
+        }
+    } while (!dValide);
 }
 
-struct Date *afficheDate(struct Date *d) {
-    return (&d);
-}
+// Retourne une date initialisé par copie
+Date creerDateParCopie() {
+    Date d;
+    initialiseDate(&d);
+    return d;
+};
 
+// Alloue dynamiquement une date, l'initialise et retourne son adresse
+Date* newDate() {
+    Date *d = (Date*)malloc(sizeof(Date));
+    initialiseDate(d);
+    return d;
+}
