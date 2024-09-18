25cbe50039c72a4b4d3fe6670ec5547c6262022e
TimoFGT <timothe.fanget@gmail.com>
2024-09-18 18:03:15 +0200

    Commmit TP fichier CSV

---
+++ C/TP3/prixTuring.c
@@ -0,0 +1,46 @@
+/**
+ Compilation
+ gcc --std=c99 -W -Wall -o prixTuring prixTuring.c
+
+ Exécution
+ ./prixTuring
+
+ Tests
+ diff out.csv turingWinners.csv
+
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// MAIN
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int main(int argc, char** argv)
+{
+	char filename[] = "turingWinners.csv";
+	char outputFilename[] = "out.csv";
+	FILE* f;
+
+	f = fopen(filename,"r");
+
+	char c="";
+	while (c != EOF) {
+		char c = fgetc(f);
+	};
+
+
+    // TODO
+
+
+	fclose(f);
+
+	return EXIT_SUCCESS;
+}
+
+
+
