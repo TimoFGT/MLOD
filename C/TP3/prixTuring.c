060f635a5c516e3009c1a206fbf4314a09f606b6
TimoFGT <timothe.fanget@gmail.com>
2024-09-20 10:02:04 +0200

    Mon premier commit sur le prix Turing

--- C/TP3/prixTuring.c
+++ C/TP3/prixTuring.c
@@ -16,6 +16,46 @@
 #include <errno.h>
 #include <assert.h>
 
+#include <stdbool.h>
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// VARIABLES
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+struct prixTuring {
+	unsigned short annee;
+	char* nom;
+	char* sujet;
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// FONCTIONS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+int numberOfWinners(FILE* farg) {
+	char c="";
+	int numeroChamp=1;
+	bool champNon = false;
+	FILE* fichier;
+	int nbLignes=0;
+	while (c != EOF) {
+		c=fgetc(fichier);
+		if (c == "\n") {
+			//On est sur une nouvelle ligne
+			while(c != "\n") {
+				
+			}
+
+		}
+	};
+	return nbLignes;
+}
+
+readWinners(FILE* ) {
+
+}
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // MAIN
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -25,12 +65,15 @@ int main(int argc, char** argv)
 	char filename[] = "turingWinners.csv";
 	char outputFilename[] = "out.csv";
 	FILE* f;
+	FILE* out;
 
 	f = fopen(filename,"r");
+	out = fopen(outputFilename,"a");
 
 	char c="";
 	while (c != EOF) {
-		char c = fgetc(f);
+		c = fgetc(f);
+		putc(c, out);
 	};
 
 
